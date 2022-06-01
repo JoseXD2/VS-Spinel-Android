9c91dbc42fb93677cb92033281b2d4ab7b1bc238
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-06-01 17:09:04 -0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -106,8 +106,8 @@ class Main extends Sprite
 		addChild(new FPS(10, 3, 0xFFFFFF));
 //		#end
 
-		#if !mobile
-//		fpsCounter = new FPS(10, 3, 0xFFFFFF);
+//		#if !mobile
+		fpsCounter = new FPS(10, 3, 0xFFFFFF);
 		addChild(fpsCounter);
 		toggleFPS(FlxG.save.data.fps);
 
